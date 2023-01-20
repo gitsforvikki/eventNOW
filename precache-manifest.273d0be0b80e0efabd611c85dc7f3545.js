bb775456c57cfdb237c06d03d2e482e27ad1134b
gitsforvikki <vk6484412@gmail.com>
2023-01-20 14:12:39 +0530

    Updates

---
+++ precache-manifest.273d0be0b80e0efabd611c85dc7f3545.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "78af3012ed5afc66e2b9",
+    "url": "/eventNOW/static/css/main.5c0fc883.chunk.css"
+  },
+  {
+    "revision": "78af3012ed5afc66e2b9",
+    "url": "/eventNOW/static/js/main.3bb5faeb.chunk.js"
+  },
+  {
+    "revision": "0d1e598e49738afe9594",
+    "url": "/eventNOW/static/js/runtime~main.5109aaae.js"
+  },
+  {
+    "revision": "b3c2f3d72fab2db5c40d",
+    "url": "/eventNOW/static/css/2.20dbaa7a.chunk.css"
+  },
+  {
+    "revision": "b3c2f3d72fab2db5c40d",
+    "url": "/eventNOW/static/js/2.bfb1db4d.chunk.js"
+  },
+  {
+    "revision": "58f1a549a56f1eadcef5",
+    "url": "/eventNOW/static/js/3.dd5dca01.chunk.js"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/eventNOW/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/eventNOW/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/eventNOW/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/eventNOW/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/eventNOW/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/eventNOW/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/eventNOW/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/eventNOW/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/eventNOW/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/eventNOW/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/eventNOW/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/eventNOW/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/eventNOW/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/eventNOW/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/eventNOW/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "270f72df47e036d9cfa11f91f0a94a60",
+    "url": "/eventNOW/static/media/arrow_left.270f72df.svg"
+  },
+  {
+    "revision": "d9ccc77c8240ad0dc2724cf333c0479e",
+    "url": "/eventNOW/static/media/arrow_right.d9ccc77c.svg"
+  },
+  {
+    "revision": "22911f0237966591317279a48b257a44",
+    "url": "/eventNOW/static/media/event-bg-2.22911f02.jpeg"
+  },
+  {
+    "revision": "d1b58f2e18edc201a0d0c4f16dccd9b0",
+    "url": "/eventNOW/index.html"
+  }
+];
